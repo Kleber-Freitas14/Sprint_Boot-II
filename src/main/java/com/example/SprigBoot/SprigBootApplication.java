f535126bfe6a6653781870cebbfbb3afe0b963c8
kleber <kleber.lo.pietro@gmail.com>
2025-10-16 16:33:50 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/SprigBoot/SprigBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.SprigBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprigBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SprigBoot/SprigBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SprigBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
